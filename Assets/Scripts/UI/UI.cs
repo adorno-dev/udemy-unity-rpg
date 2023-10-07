a1029a7b7a69d22d39e49bdb62e8c119bdf9c170
adorno-dev <adorno.dev@gmail.com>
2023-10-07 09:59:35 -0300

    Switch UI with keyboard

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -2,12 +2,33 @@ using UnityEngine;
 
 public class UI : MonoBehaviour
 {
+    [SerializeField] private GameObject characterUI;
+    [SerializeField] private GameObject skillTreeUI;
+    [SerializeField] private GameObject craftUI;
+    [SerializeField] private GameObject optionsUI;
+
     public UI_ItemToolTip itemToolTip;
     public UI_StatToolTip statToolTip;
 
     void Start()
     {
-        // itemToolTip = GetComponentInChildren<UI_ItemToolTip>();
+        itemToolTip.gameObject.SetActive(false);
+        statToolTip.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+            SwitchWithKeyTo(characterUI);
+
+        if (Input.GetKeyDown(KeyCode.B))
+            SwitchWithKeyTo(craftUI);
+
+        if (Input.GetKeyDown(KeyCode.K))
+            SwitchWithKeyTo(skillTreeUI);
+
+        if (Input.GetKeyDown(KeyCode.O))
+            SwitchWithKeyTo(optionsUI);
     }
 
     public void SwitchTo(GameObject menu)
@@ -23,5 +44,16 @@ public class UI : MonoBehaviour
             else
                 current.gameObject.SetActive(false);
         }
+    }
+
+    public void SwitchWithKeyTo(GameObject menu)
+    {
+        if (menu != null && menu.activeSelf)
+        {
+            menu.SetActive(false);
+            return;
+        }
+
+        SwitchTo(menu);
     }
 }
