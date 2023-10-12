6aa7662c4c6a18a847ec982be3a78dc2fba33a6a
adorno-dev <adorno.dev@gmail.com>
2023-10-12 11:17:24 -0300

    Arrow controller

---
+++ Assets/Scripts/Controllers/Arrow_Controller.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class Arrow_Controller : MonoBehaviour
+{
+    [SerializeField] private int damage;
+    [SerializeField] private string targetLayerName = "Player";
+
+    [SerializeField] private float xVelocity;
+    [SerializeField] private Rigidbody2D rb;
+
+    [SerializeField] private bool canMove;
+    [SerializeField] private bool flipped;
+
+    private void Update()
+    {
+        if (canMove)
+            rb.velocity = new Vector2(xVelocity, rb.velocity.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer(targetLayerName))
+        {
+            other.GetComponent<CharacterStats>()?.TakeDamage(damage);
+
+            StuckInto(other);
+        }
+        else if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+            StuckInto(other);
+    }
+
+    private void StuckInto(Collider2D other)
+    {
+        GetComponentInChildren<ParticleSystem>().Stop();
+        GetComponent<CapsuleCollider2D>().enabled = false;
+
+        canMove = false;
+        rb.isKinematic = true;
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        transform.parent = other.transform;
+
+        Destroy(gameObject, Random.Range(5, 7));
+    }
+
+    public void FlipArrow()
+    {
+        if (flipped)
+            return;
+
+        xVelocity = xVelocity * -1;
+        flipped = true;
+        transform.Rotate(0, 180, 0);
+
+        targetLayerName = "Enemy";
+    }
+}
--- Assets/Scripts/Player/PlayerCounterAttackState.cs
+++ Assets/Scripts/Player/PlayerCounterAttackState.cs
@@ -25,12 +25,17 @@ public class PlayerCounterAttackState : PlayerState
 
         foreach (var hit in colliders)
         {
+            if (hit.GetComponent<Arrow_Controller>() != null)
+            {
+                hit.GetComponent<Arrow_Controller>().FlipArrow();
+                SuccessfulCounterAttack();
+            }
+
             if (hit.GetComponent<Enemy>() != null)
             {
                 if (hit.GetComponent<Enemy>().CanBeStunned())
                 {
-                    stateTimer = 10; // any value bigger than 1
-                    player.anim.SetBool("SuccessfulCounterAttack", true);
+                    SuccessfulCounterAttack();
 
                     player.skill.parry.UseSkill(); // going to use to restore health on parry
 
@@ -45,6 +50,12 @@ public class PlayerCounterAttackState : PlayerState
 
         if (stateTimer < 0 || triggerCalled)
             stateMachine.ChangeState(player.idleState);
+    }
+
+    private void SuccessfulCounterAttack()
+    {
+        stateTimer = 10; // any value bigger than 1
+        player.anim.SetBool("SuccessfulCounterAttack", true);
     }
 
     public override void Exit()
